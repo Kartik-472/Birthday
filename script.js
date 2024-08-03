bba687c0e2c9913ac2c410e0927ffcb8fc9da4f3
Kartik-472 <153933263+Kartik-472@users.noreply.github.com>
2024-08-03 18:30:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    window.location.href = 'https://youtu.be/xMHJGd3wwZk?si=K3ircMP8UFsgLDDW'; // Rickroll link
+});
